16dfad93b0431cde14034ec2e568e271befaf416
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-26 17:32:42 +0530

    created expense model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField()
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
