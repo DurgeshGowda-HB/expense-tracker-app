e514fde88b9b45947d5841a7ef868b0870d837ea
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-27 18:40:36 +0530

    created expense form using ModelForm

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ExpenseForm
 
 def home(request):
     return render(request, 'tracker/home.html')
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = ExpenseForm()
+
+    return render(request, 'tracker/add_expense.html', {'form': form})
