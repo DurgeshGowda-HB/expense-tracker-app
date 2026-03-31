297525e46133092a431d75205f01c851a61206e6
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-31 11:33:17 +0530

    updated Expense model with category choices

--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Travel', 'Travel'),
+        ('Shopping', 'Shopping'),
+        ('Bills', 'Bills'),
+        ('Other', 'Other'),
+    ]
+
     title = models.CharField(max_length=100)
     amount = models.FloatField()
     date = models.DateField()
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.title
